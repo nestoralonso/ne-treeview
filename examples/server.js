5c92a0fecb64349a3d61248f9aaaf09e0c286586
NOM <n@n>
2016-09-08 15:36:49 -0500

    fixed a little bug that prevented index.html of being accessed in the example

--- examples/server.js
+++ examples/server.js
@@ -5,13 +5,14 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var WebpackConfig = require('./webpack.config');
 
 var app = express();
-console.log(__dirname);
 app.use(webpackDevMiddleware(webpack(WebpackConfig), {
   publicPath: '/__build__/',
   stats: {
     colors: true
   }
 }));
+
+app.use(express.static(__dirname));
 
 app.listen(8080, function() {
   console.log('Server listening on 💀 ⚡ http://localhost:8080 ⚡ 💀, Ctrl+C to stop');

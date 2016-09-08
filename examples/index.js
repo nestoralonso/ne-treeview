ee273ce700ca3ec43a9a011a61d8587ef08be1ae
NOM <n@n>
2016-09-08 15:37:40 -0500

    added two more trees to the example

--- examples/index.js
+++ examples/index.js
@@ -32,9 +32,48 @@ let root = {
   ]
 };
 
+const threeNodes = {
+  label: 'parent',
+  items: [
+    {
+      label: 'child-1'
+    },
+    {
+      label: 'child-2'
+    }]
+};
+
+const threeLevels = {
+  label: 'c-0',
+  items: [
+    {
+      label: 'c-0-1'
+    },
+    {
+      label: 'c-0-2',
+      items: [
+        {label: 'c-0-2-0'},
+        {label: 'c-0-2-1'}
+      ]
+    }]
+};
+
+const treeStyle = {float: 'left', width: 200, border: '1px dotted darkgrey'};
+
 ReactDOM.render(
-  <div style={{width: 600, margin: '0 auto'}}>
-    <TreeView root={root} />
+  <div className="row">
+    <div className="col-md-4">
+      <h2>The original example</h2>
+      <TreeView root={root} style={treeStyle} />
+    </div>
+    <div className="col-md-4">
+      <h2>Minimal example</h2>
+      <TreeView root={threeNodes} style={treeStyle}/>
+    </div>
+    <div className="col-md-4">
+      <h2>Other example</h2>
+      <TreeView root={threeLevels} style={treeStyle}/>
+    </div>
   </div>,
   document.getElementById('root')
 );
